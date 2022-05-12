17a80a32a04be54c1e7ded2170a6f41b7a6b2e2a
adamkvitek <adamkvitek@gmail.com>
2022-05-12 19:23:02 +0200

    Add class inheritance with typed metavariables examples

---
+++ docs/class_inheritance_typed_metavariables/app.java
@@ -0,0 +1,18 @@
+package example;
+
+public class App {
+    public void check(String s) {
+        if (is_bad(s)) {
+            // rule-id: throw-exception-example
+            throw new ExampleException(s);
+        }
+    }
+
+    public void check2(String s) {
+        if (is_bad(s)) {
+            // ok: throw-exception-example
+            throw new BadRequest(s);
+        }
+    }
+}
+
