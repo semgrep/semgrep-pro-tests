cf28b7f229e5a7160c819803f6a6381320b10f1e
adamkvitek <adamkvitek@gmail.com>
2022-05-12 20:16:10 +0200

    Add taint tracking example

---
+++ docs/taint_tracking/constants.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public final class Constants {
+    public static final double PI = 3.14159;
+    public static final double PLANCK_CONSTANT = 6.62606896e-34;
+    public static final String EMPLOYEE_TABLE_NAME = "Employees";
+}
