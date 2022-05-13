c42d5ce84245404245e55195a35b92d9f8fec8ed
Emma Jin <emjin@users.noreply.github.com>
2022-05-13 09:29:59 -0700

    Modify constant propagation example to illustrate multifile more clearly

--- docs/constant_propagation_propagating_values/app.java
+++ docs/constant_propagation_propagating_values/app.java
@@ -6,17 +6,17 @@ public class App {
     public void example(String user_input) {
 
         // ok:
-        dangerous("Select * FROM " + user_input);
+        dangerous(user_input);
 
         // deep: dangerous-call-to-employees
-        dangerous("Select * FROM " + EMPLOYEE_TABLE_NAME);
+        dangerous(EMPLOYEE_TABLE_NAME);
         
         String table_name = "Employees";
         // deep: dangerous-call-to-employees   
-        dangerous("Select * FROM " + table_name);
+        dangerous(table_name);
 
         // rule-id: dangerous-call-to-employees
-        dangerous("Select * FROM Employees");
+        dangerous("Employees");
     }
 
 }
